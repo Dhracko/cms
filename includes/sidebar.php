caf81cb8a4dc02e1cc51769aea739fe8e3ab8e6a
Dhracko <dracko@hotmail.co.uk>
2021-12-01 19:25:11 +0000

    login functionality added

--- includes/sidebar.php
+++ includes/sidebar.php
@@ -18,6 +18,30 @@
     </div>
 
 
+     <!-- Login -->
+     <div class="well">
+        <h4>Blog Search</h4>
+        <form action="includes/login.php" method="POST">
+            <div class="form-group">
+                <input name="username" type="text" class="form-control" placeholder="Enter Username">
+                
+            </div>
+            <div class="input-group">
+                <input name="password" type="password" class="form-control" placeholder="Enter Password">
+                <span class="input-group-btn">
+                    <button class="btn btn-primary" name="login" type="submit">Submit</button>
+
+
+
+
+                </span>
+                
+            </div>
+        </form>
+        <!--search form -->
+        <!-- /.input-group -->
+    </div>
+
 
 
 
