12dd942925de26f6b4c42aa60640d4eb9b51322d
Dhracko <dracko@hotmail.co.uk>
2021-12-02 20:24:08 +0000

    login and logout system

--- includes/login.php
+++ includes/login.php
@@ -6,14 +6,14 @@ if (isset($_POST['login'])) {
 
     $username = $_POST['username'];
     $password = $_POST['password'];
-    
+
     $username = mysqli_real_escape_string($connection, $username);
     $password = mysqli_real_escape_string($connection, $password);
 
 
     $query = "SELECT * FROM users WHERE username= '{$username}'";
     $select_user_query = mysqli_query($connection, $query);
-    if(!$select_user_query) {
+    if (!$select_user_query) {
         die("QUERY FAILED" . mysqli_error($connection));
     }
 
@@ -25,27 +25,19 @@ if (isset($_POST['login'])) {
         $db_user_firstname = $row['user_firstname'];
         $db_user_lastname = $row['user_lastname'];
         $db_user_role = $row['user_role'];
-
     }
 
-    if($username !== $db_username && $password !== $db_user_password ) {
-        header("Location: ../index.php");
-    } else if ($username == $db_username && $password == $db_user_password ) {
+    if ($username === $db_username && $password === $db_user_password) {
 
         $_SESSION['username'] = $db_username;
         $_SESSION['firstname'] = $db_user_firstname;
         $_SESSION['lastname'] = $db_user_lastname;
         $_SESSION['user_role'] = $db_user_role;
 
-
-
         header("Location: ../admin");
     } else {
         header("Location: ../index.php");
     }
-
-
-
 }
 
 
