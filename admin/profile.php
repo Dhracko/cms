0ae4c5e2e9714cdce7ff84936bae3bc5cfe96128
Dhracko <dracko@hotmail.co.uk>
2021-12-06 19:24:57 +0000

    Profile page

--- admin/profile.php
+++ admin/profile.php
@@ -1,12 +1,15 @@
-<?php
-if(isset($_GET['edit_user'])){
+<?php include "includes/admin_header.php" ?>
 
-    $the_user_id = $_GET['edit_user'];
+<?php 
+if(isset($_SESSION['username'])) {
+    $username = $_SESSION['username'];
 
-    $query = "SELECT * FROM users WHERE user_id = $the_user_id ";
-    $select_users_query = mysqli_query($connection, $query);
+    $query = "SELECT * FROM users WHERE username = '{$username}' ";
 
-    while ($row = mysqli_fetch_assoc($select_users_query)) {
+    $select_user_profile_query = mysqli_query($connection, $query);
+
+    while($row = mysqli_fetch_array($select_user_profile_query)) {
+
         $user_id        = $row['user_id'];
         $username       = $row['username'];
         $user_password  = $row['user_password'];
@@ -15,18 +18,13 @@ if(isset($_GET['edit_user'])){
         $user_email     = $row['user_email'];
         $user_image     = $row['user_image'];
         $user_role      = $row['user_role'];
-
     }
-
-
-
-
 
 }
 
+?>
 
-
-
+<?php
 if (isset($_POST['edit_user'])) {
 
 
@@ -54,15 +52,29 @@ if (isset($_POST['edit_user'])) {
     $query .= "username = '{$username}', ";
     $query .= "user_email = '{$user_email}', ";
     $query .= "user_password   = '{$user_password}' ";
-    $query .= "WHERE user_id = {$the_user_id} ";
+    $query .= "WHERE username = '{$username}' ";
 
     $edit_user_query = mysqli_query($connection, $query);
 
     confirmQuery($edit_user_query);
 }
+
 ?>
 
+<div id="wrapper">
 
+    <!-- Navigation -->
+    <?php include "includes/admin_navigation.php" ?>
+
+    <div id="page-wrapper">
+        <div class="container-fluid">
+            <!-- Page Heading -->
+            <div class="row">
+                <div class="col-lg-12">
+                    <h1 class="page-header">
+                        Welcome to Admin
+                        <small>Author</small>
+                    </h1>
 <form action="" method="post" enctype="multipart/form-data">
 
     <div class="form-group">
@@ -75,14 +87,14 @@ if (isset($_POST['edit_user'])) {
     </div>
     <div class="form-group">
 <select name="user_role" id="user_role">
-            <option value="subscriber"><?php echo $user_role; ?></option>
+            <option value="Subscriber"><?php echo $user_role; ?></option>
 <?php 
 
-if($user_role == 'admin'){
-    echo "<option value='subscriber'>subscriber</option>";
+if($user_role == 'Admin'){
+    echo "<option value='Subscriber'>Subscriber</option>";
 } else {
 
-    echo "<option value='admin'>admin</option>";
+    echo "<option value='Admin'>Admin</option>";
 }
 
 
@@ -114,8 +126,23 @@ if($user_role == 'admin'){
     </div>
 
     <div class="form-group">
-        <input class="btn btn-primary" type="submit" name="edit_user" value="Edit User">
+        <input class="btn btn-primary" type="submit" name="edit_user" value="Update Profile">
     </div>
 
 
 </form>
+                    
+
+
+                </div>
+            </div>
+            <!-- /.row -->
+
+        </div>
+        <!-- /.container-fluid -->
+
+    </div>
+    <!-- /#page-wrapper -->
+
+
+    <?php include "includes/admin_footer.php" ?>
